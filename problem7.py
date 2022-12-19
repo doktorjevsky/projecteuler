955c4df05d1a45d1a41463030b67b4a283e71ff8
doktorjevsky <97901243+doktorjevsky@users.noreply.github.com>
2022-12-19 22:36:22 +0100

    :)

---
+++ problem7.py
@@ -0,0 +1,18 @@
+
+"""
+What is the 10 001st prime number?
+"""
+
+def sieve(n):
+    bools = list(True for _ in range(n+1))
+    primes = []
+    for i in range(2, len(bools)):
+        if bools[i]:
+            primes.append(i)
+            for j in range(2*i, len(bools), i):
+                bools[j] = False
+    return primes
+
+
+PRIMES = sieve(11*10000)
+print(PRIMES[10000])
